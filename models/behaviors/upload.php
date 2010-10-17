e4117c8a64c85a78571c2fbfb1951f195e3412d1
nathan <nathan@tylerdigital.com>
2010-10-17 15:43:17 -0700

    Added missing DS when preparing for deletion

--- models/behaviors/upload.php
+++ models/behaviors/upload.php
@@ -661,7 +661,7 @@ class UploadBehavior extends ModelBehavior {
 		$this->__filesToRemove[$model->alias] = array();
 		$this->__filesToRemove[$model->alias][] = ROOT . DS . APP_DIR . DS . $this->settings[$model->alias][$field]['path'] . $data[$model->alias][$options['fields']['dir']] . DS . $data[$model->alias][$field];
 		foreach ($options['thumbsizes'] as $style => $geometry) {
-			$this->__filesToRemove[$model->alias][] = ROOT . DS . APP_DIR . DS . $this->settings[$model->alias][$field]['path'] . $data[$model->alias][$options['fields']['dir']] . $style . '_' . $data[$model->alias][$field];
+			$this->__filesToRemove[$model->alias][] = ROOT . DS . APP_DIR . DS . $this->settings[$model->alias][$field]['path'] . $data[$model->alias][$options['fields']['dir']] . DS . $style . '_' . $data[$model->alias][$field];
 		}
 		return $this->__filesToRemove;
 	}
