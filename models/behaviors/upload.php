e247f4bd544aee652648c74a7634a13d80913073
Jose Diaz-Gonzalez <myaccounts@savant.be>
2012-06-10 15:24:16 -0400

    Add support for primaryKey in thumbnail renaming

--- models/behaviors/upload.php
+++ models/behaviors/upload.php
@@ -902,8 +902,8 @@ class UploadBehavior extends ModelBehavior {
 		}
 
 		$fileName = str_replace(
-			array('{size}', '{filename}'),
-			array($size, $pathInfo['filename']),
+			array('{size}', '{filename}', '{primaryKey}'),
+			array($size, $pathInfo['filename'], $model->id),
 			$this->settings[$model->alias][$field]['thumbnailName']
 		);
 
@@ -1247,8 +1247,8 @@ class UploadBehavior extends ModelBehavior {
 
 		foreach ($options['thumbnailSizes'] as $size => $geometry) {
 			$fileName = str_replace(
-				array('{size}', '{filename}'),
-				array($size, $pathInfo['filename']),
+				array('{size}', '{filename}', '{primaryKey}'),
+				array($size, $pathInfo['filename'], $model->id),
 				$options['thumbnailName']
 			);
 
