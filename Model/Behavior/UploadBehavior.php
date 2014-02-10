5bcb232ea296a71bbc1a72a771fdbffcd4b97c06
Ricardo Silva Veloso <ricvelozo@gmail.com>
2014-02-10 01:33:44 -0200

    Fix to when the file is stored out of webroot

--- Model/Behavior/UploadBehavior.php
+++ Model/Behavior/UploadBehavior.php
@@ -331,7 +331,10 @@ class UploadBehavior extends ModelBehavior {
 
 				$settingsTemp = $this->settings[$model->alias][$field];
 				$settingsTemp['path'] = $settingsTemp['dir'];
-				$dir = substr($this->_path($model, $field, $settingsTemp), 1);
+				$dir = $this->_path($model, $field, $settingsTemp);
+				if (substr($dir, 0, 1) === DIRECTORY_SEPARATOR) {
+					$dir = substr($dir, 1);
+				}
 			}
 			$tmp = $this->runtime[$model->alias][$field]['tmp_name'];
 			$filePath = $path . $model->data[$model->alias][$field];
