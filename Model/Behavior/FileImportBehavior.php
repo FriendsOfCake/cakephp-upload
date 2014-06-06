ca4bc7acbefce11bac54e5b9b4b5dcc8c4771945
Jose Diaz-Gonzalez <josegonzalez@users.noreply.github.com>
2014-06-06 12:30:26 -0400

    Fix FileImportBehavior

--- Model/Behavior/FileImportBehavior.php
+++ Model/Behavior/FileImportBehavior.php
@@ -2,8 +2,4 @@
 App::uses('UploadBehavior', 'Upload.Model/Behavior');
 class FileImportBehavior extends UploadBehavior {
 
-	public function handleUploadedFile($modelAlias, $field, $tmp, $filePath) {
-		return !rename($tmp, $filePath);
-	}
-
 }
