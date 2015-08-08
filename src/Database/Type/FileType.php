f33dbc60fe4ed7298878a9edf714f76251fbe342
Jose Diaz-Gonzalez <myaccounts@savant.be>
2015-08-08 17:10:57 -0400

    Add docblock

--- src/Database/Type/FileType.php
+++ src/Database/Type/FileType.php
@@ -5,6 +5,15 @@ use Cake\Database\Type;
 
 class FileType extends Type
 {
+    /**
+     * Marshalls flat data into PHP objects.
+     *
+     * Most useful for converting request data into PHP objects
+     * that make sense for the rest of the ORM/Database layers.
+     *
+     * @param mixed $value The value to convert.
+     * @return mixed Converted value.
+     */
     public function marshal($value)
     {
         return $value;
