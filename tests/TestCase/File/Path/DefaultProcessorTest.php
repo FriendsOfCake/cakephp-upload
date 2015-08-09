35e75cd74259aa989ef8411f289dc8a588dc3dda
Jose Diaz-Gonzalez <myaccounts@savant.be>
2015-08-09 00:17:03 -0400

    Add DefaultProcessorTest

---
+++ tests/TestCase/File/Path/DefaultProcessorTest.php
@@ -0,0 +1,20 @@
+<?php
+namespace Josegonzalez\Upload\Test\TestCase\File\Path;
+
+use Cake\TestSuite\TestCase;
+use Josegonzalez\Upload\File\Path\DefaultProcessor;
+use Josegonzalez\Upload\File\Path\ProcessorInterface;
+
+class DefaultProcessorTest extends TestCase
+{
+    public function testInvoke()
+    {
+        $table = $this->getMock('Cake\ORM\Table');
+        $entity = $this->getMock('Cake\ORM\Entity');
+        $data = ['name' => 'filename'];
+        $field = 'field';
+        $settings = [];
+        $processor = new DefaultProcessor($table, $entity, $data, $field, $settings);
+        $this->assertInstanceOf('Josegonzalez\Upload\File\Path\ProcessorInterface', $processor);
+    }
+}
