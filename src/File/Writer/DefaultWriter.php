fd0a6be4b06f8885a49cf0ffacc27dcf85612ddc
Jose Diaz-Gonzalez <myaccounts@savant.be>
2015-08-08 20:07:14 -0400

    Add more docblocks and use a better exception

--- src/File/Writer/DefaultWriter.php
+++ src/File/Writer/DefaultWriter.php
@@ -2,14 +2,22 @@
 namespace Josegonzalez\Upload\File\Writer;
 
 use Cake\Utility\Hash;
-use Exception;
 use League\Flysystem\AdapterInterface;
 use League\Flysystem\Adapter\Local;
 use League\Flysystem\FileNotFoundException;
 use League\Flysystem\Filesystem;
+use League\Flysystem\FilesystemInterface;
+use UnexpectedValueException;
 
 class DefaultWriter
 {
+    /**
+     * Writes a set of files to an output
+     *
+     * @param array $files the files being written out
+     * @param string $field the field for which data will be saved
+     * @param array $settings the settings for the current field
+     */
     public function __invoke($files, $field, $settings)
     {
         $filesystem = $this->getFilesystem($field, $settings);
@@ -21,7 +29,16 @@ class DefaultWriter
         return true;
     }
 
-    public function writeFile($filesystem, $file, $path)
+    /**
+     * Writes a set of files to an output
+     *
+     * @param League\Flysystem\FilesystemInterface $filesystem a filesystem wrapper
+     * @param string $file a full path to a temp file
+     * @param string $path that path to which the file should be written
+     * @param array $settings the settings for the current field
+     * @return bool
+     */
+    public function writeFile(FilesystemInterface $filesystem, $file, $path)
     {
         $success = false;
         $stream = fopen($file, 'r+');
@@ -36,7 +53,15 @@ class DefaultWriter
         return $success;
     }
 
-    public function deletePath($filesystem, $path)
+    /**
+     * Deletes a path from a filesystem
+     *
+     * @param League\Flysystem\FilesystemInterface $filesystem a filesystem writer
+     * @param string $path the path that should be deleted
+     * @param array $settings the settings for the current field
+     * @return void
+     */
+    public function deletePath(FilesystemInterface $filesystem, $path)
     {
         try {
             $filesystem->delete($path);
@@ -45,6 +70,13 @@ class DefaultWriter
         }
     }
 
+    /**
+     * Retrieves a configured filesystem for the given field
+     *
+     * @param string $field the field for which data will be saved
+     * @param array $settings the settings for the current field
+     * @return League\Flysystem\FilesystemInterface
+     */
     public function getFilesystem($field, array $settings = [])
     {
         $adapter = new Local(Hash::get($settings, 'rootDir', ROOT . DS));
@@ -59,6 +91,6 @@ class DefaultWriter
             ]));
         }
 
-        throw new Exception(sprintf("Invalid Adapter for field %s", $field));
+        throw new UnexpectedValueException(sprintf("Invalid Adapter for field %s", $field));
     }
 }
