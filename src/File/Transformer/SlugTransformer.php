f2b4b0d08589342b38d16c01490dd87c513b31ce
ADmad <admad.coder@gmail.com>
2015-09-08 01:12:47 +0530

    Add SlugTransformer

---
+++ src/File/Transformer/SlugTransformer.php
@@ -0,0 +1,29 @@
+<?php
+namespace Josegonzalez\Upload\File\Transformer;
+
+use Cake\Utility\Inflector;
+use Josegonzalez\Upload\File\Transformer\DefaultTransformer;
+
+class DefaultTransformer extends DefaultTransformer
+{
+    /**
+     * Creates a set of files from the initial data and returns them as key/value
+     * pairs, where the path on disk maps to name which each file should have.
+     * The file name will be sluggified (using Inflector::slug()) and lowercased.
+     *
+     * Example:
+     *
+     * ```
+     *   [
+     *     '/tmp/path/to/file/on/disk' => 'file.pdf',
+     *     '/tmp/path/to/file/on/disk-2' => 'file-preview.png',
+     *   ]
+     * ```
+     *
+     * @return array key/value pairs of temp files mapping to their names
+     */
+    public function transform()
+    {
+        return [$this->data['tmp_name'] => strtolower(Inflector::slug($this->data['name'], '-'))];
+    }
+}
