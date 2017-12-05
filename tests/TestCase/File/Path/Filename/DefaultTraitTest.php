1df2f932e1ceeeb61858b8191dade124f858235e
Matthias Freund <matthias88freund@gmail.com>
2017-12-05 17:02:59 +0100

    tests: add nameCallback function backwards compatibility test

--- tests/TestCase/File/Path/Filename/DefaultTraitTest.php
+++ tests/TestCase/File/Path/Filename/DefaultTraitTest.php
@@ -23,6 +23,15 @@ class DefaultTraitTest extends TestCase
         $this->assertEquals('filename', $mock->filename());
 
         $mock = $this->getMockForTrait('Josegonzalez\Upload\File\Path\Filename\DefaultTrait');
+        $mock->settings = [
+            'nameCallback' => function ($data, $settings) {
+                return $data;
+            },
+        ];
+        $mock->data = ['name' => 'filename'];
+        $this->assertEquals(['name' => 'filename'], $mock->filename());
+
+        $mock = $this->getMockForTrait('Josegonzalez\Upload\File\Path\Filename\DefaultTrait');
         $mock->entity = $this->getMockBuilder('Cake\ORM\Entity')->getMock();
         $mock->table = $this->getMockBuilder('Cake\ORM\Table')->getMock();
         $mock->field = 'field';
