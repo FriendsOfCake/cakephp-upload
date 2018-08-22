5468717b86867f1b8db9cc000911de39daae6ec8
AlexAlexandru <alexandru_mandric@yahoo.com>
2018-08-22 21:01:01 +0300

    added base64 validation

--- src/Validation/DefaultValidation.php
+++ src/Validation/DefaultValidation.php
@@ -2,6 +2,7 @@
 
 namespace Josegonzalez\Upload\Validation;
 
+use Josegonzalez\Upload\Validation\Traits\Base64ValidationTrait;
 use Josegonzalez\Upload\Validation\Traits\ImageValidationTrait;
 use Josegonzalez\Upload\Validation\Traits\UploadValidationTrait;
 
@@ -9,4 +10,5 @@ class DefaultValidation
 {
     use ImageValidationTrait;
     use UploadValidationTrait;
+    use Base64ValidationTrait;
 }
